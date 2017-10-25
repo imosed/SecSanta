0b39c4ccbcdb99d22bd40ac7f330963df6366916
Jared <imosed@users.noreply.github.com>
2017-10-25 01:17:56 -0400

    Update index.php

--- index.php
+++ index.php
@@ -15,21 +15,5 @@
         <input type="text" class="part-name small match-code" placeholder="Match (optional)" name="mcode">
         <input type="button" class="hfe" id="hlpb"></input>
     </form>
-    <!--<div id="mcinst" style="display: none;">
-        <p>Your match code consists of the following:</p>
-        <ul>
-            <li>First letter of match's first name</li>
-            <li>First letter of match's last name</li>
-            <li>Last letter of match's last name</li>
-            <li>Number of non-space characters in match's full name</li>
-        </ul>
-        <p>This string is case insensitive.</p>
-        <p>So, for example, if your match's name is Gary Johnson, your match code would be &quot;GJN11&quot;.</p>
-    </div>-->
-</div>
-<!--<script type="text/javascript">
-    document.getElementById('hlpb').addEventListener('mouseup', function() { document.getElementById('mcinst').style.display = 'block'; }, false);
-    document.getElementById('hlpb').addEventListener('touchend', function() { document.getElementById('mcinst').style.display = 'block'; }, false);
-</script>-->
 </body>
 </html>
